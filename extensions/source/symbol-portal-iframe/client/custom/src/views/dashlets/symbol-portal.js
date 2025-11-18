9c2eed9654450734322b9f51f3ddafd06e644caf
Kieran Hiett <kieranhiett@Kierans-MacBook-Pro.local>
2025-11-18 15:24:31 +0000

    Bump SymbolPortalIframe to 1.0.1

--- extensions/source/symbol-portal-iframe/client/custom/src/views/dashlets/symbol-portal.js
+++ extensions/source/symbol-portal-iframe/client/custom/src/views/dashlets/symbol-portal.js
@@ -1,18 +1,17 @@
-define(['views/dashlets/abstract/base'], (BaseView) => {
+Espo.define('custom:views/dashlets/symbol-portal', ['views/dashlets/abstract/base'], function (BaseView) {
 
-    return class SymbolPortalDashlet extends BaseView {
+    return BaseView.extend({
 
-        name = 'SymbolPortal';
+        name: 'SymbolPortal',
 
-        templateContent = `
-            <div class="symbol-iframe-dashlet" style="height:100%;min-height:420px;">
-                <iframe
-                    src="https://symbol.ratedpeople.com/login.html"
-                    style="border:0;width:100%;height:100%;"
-                    loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"
-                ></iframe>
-            </div>
-        `;
-    };
+        templateContent:
+            '<div class="symbol-iframe-dashlet" style="height:100%;min-height:420px;">' +
+                '<iframe '
+                    + 'src="https://symbol.ratedpeople.com/login.html" '
+                    + 'style="border:0;width:100%;height:100%;" '
+                    + 'loading="lazy" '
+                    + 'referrerpolicy="no-referrer-when-downgrade">'
+                + '</iframe>' +
+            '</div>'
+    });
 });
