fb5fa60972b56c8c812526d060b3936184a27333
Kieran Hiett <kieranhiett@Kierans-MacBook-Pro.local>
2025-11-18 17:07:01 +0000

    Point Symbol dashlet at index.html

--- extensions/source/symbol-portal-iframe/client/custom/src/views/dashlets/symbol-portal.js
+++ extensions/source/symbol-portal-iframe/client/custom/src/views/dashlets/symbol-portal.js
@@ -7,7 +7,7 @@ Espo.define('custom:views/dashlets/symbol-portal', ['views/dashlets/abstract/bas
         templateContent:
             '<div class="symbol-iframe-dashlet" style="height:100%;min-height:420px;">' +
                 '<iframe '
-                    + 'src="https://symbol.ratedpeople.com/login.html" '
+                    + 'src="https://symbol.ratedpeople.com/index.html" '
                     + 'style="border:0;width:100%;height:100%;" '
                     + 'loading="lazy" '
                     + 'referrerpolicy="no-referrer-when-downgrade">'
