5157e9a6ea913da09f496dbf75a255574b2c4f16
Kieran Hiett <kieranhiett@Kierans-MacBook-Pro.local>
2025-11-20 15:06:06 +0000

    harden rollback Test External bottom panel view

--- espo-live/client/custom/src/views/account/panels/test-external.js
+++ espo-live/client/custom/src/views/account/panels/test-external.js
@@ -2,25 +2,66 @@ define('custom:views/account/panels/test-external', ['views/record/panels/bottom
 
     return Dep.extend({
 
+        // Should match the panel key in bottomPanelsDetail.json
         name: 'testExternal',
 
-        // Use the standard bottom panel template for now
-        // so we know tpl isn't breaking anything.
-        template: 'record/panels/bottom',
+        template: 'custom:account/panels/test-external',
 
         init: function () {
             Dep.prototype.init.call(this);
+            this.rows = [];
         },
 
         setup: function () {
             Dep.prototype.setup.call(this);
 
-            console.log('TestExternal bottom panel initialised for Account', this.model && this.model.id);
+            // Reload panel data when the record changes, just in case
+            if (this.model) {
+                this.listenTo(this.model, 'after:save', this.loadData, this);
+            }
+
+            this.loadData();
+        },
+
+        loadData: function () {
+            var id = this.model && this.model.id;
+            var self = this;
+
+            if (!id) {
+                this.rows = [];
+                this.reRender();
+                return;
+            }
+
+            this.ajaxGetRequest('TestExternal/account/' + id)
+                .then(function (data) {
+                    try {
+                        self.rows = data && data.rows ? data.rows : [];
+                        self.reRender();
+                    } catch (e) {
+                        console.error('TestExternal panel render error (then):', e);
+                    }
+                })
+                .catch(function (e) {
+                    console.error('TestExternal panel error (ajax):', e);
+                    self.rows = [];
+                    try {
+                        self.reRender();
+                    } catch (e2) {
+                        console.error('TestExternal panel render error (catch):', e2);
+                    }
+                });
         },
 
         data: function () {
-            // Just return parent data – no rows, no custom logic yet.
-            return Dep.prototype.data.call(this) || {};
+            var parentData = Dep.prototype.data.call(this) || {};
+            var rows = this.rows || [];
+
+            return _.extend({}, parentData, {
+                rows: rows,
+                hasRows: rows.length > 0,
+                firstRow: rows.length ? rows[0] : null
+            });
         },
     });
 });
